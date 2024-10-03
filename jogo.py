32c6c893b2ab9856c1bcc71f8baba0f9bbf8372e
Guilherme <guilhermefreschix@gmail.com>
2024-10-03 17:59:40 -0300

    Adiciona classe personagem

---
+++ jogo.py
@@ -0,0 +1,34 @@
+from colorama import Fore, Back, Style
+import random
+
+# Personagem: classe mãe
+# Herói: controlado pelo usuário
+# Inimigo: adversário do usuário
+
+class Personagem:
+    def __init__(self, nome, vida, nivel):
+        self.__nome = nome
+        self.__vida = vida
+        self.__nivel = nivel
+        
+    def get_nome(self):
+        return self.__nome
+    
+    def get_vida(self):
+        return self.__vida
+    
+    def get_nivel(self):
+        return self.__nivel
+    
+    def exibir_detalhes(self):
+        return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNível: {self.get_nivel()}"
+    
+    def receber_ataque(self, dano):
+        self.__vida -= dano    
+        if self.__vida < 0:
+            self.__vida = 0
+    
+    def atacar(self, alvo):
+        dano = random.randint(self.get_nivel() * 2, self.get_nivel() * 4)
+        alvo.receber_ataque(dano)
+        print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
