df4f3fe447f4c3c1856099bfe776fd948c09ec75
Guilherme <guilhermefreschix@gmail.com>
2024-10-03 18:17:46 -0300

    Adiciona classe herói

--- jogo.py
+++ jogo.py
@@ -32,3 +32,19 @@ class Personagem:
         dano = random.randint(self.get_nivel() * 2, self.get_nivel() * 4)
         alvo.receber_ataque(dano)
         print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
+        
+class Heroi(Personagem):
+    def __init__(self, nome, vida, nivel, habilidade):
+        super().__init__(nome, vida, nivel)
+        self.__habilidade = habilidade
+    
+    def get_habilidade(self):
+        return self.__habilidade
+    
+    def exibir_detalhes(self):
+        return Fore.BLUE + f"{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}\n"
+    
+    def ataque_especial(self, alvo):
+        dano = random.randint(self.get_nivel() * 5, self.get_nivel() * 8) 
+        alvo.receber_ataque(dano)
+        print(f"{self.get_nome()} usou a habilidade especial {self.get_habilidade()} em {alvo.get_nome()} e causou {dano} de dano!")
